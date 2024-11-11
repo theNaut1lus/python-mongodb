bfc861886e6d67253c50934da11dddff20fa1106
Sidney Aulakh <sidneyaulakh@gmail.com>
2024-11-11 23:04:40 +1100

    fix depreciated event calls by using lifespan

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from dotenv import dotenv_values
 from pymongo import MongoClient
@@ -5,19 +6,26 @@ from routes import router as book_router
 
 config = dotenv_values(".env")
 
-app = FastAPI()
 
-
-@app.on_event("startup")
 def startup_b_client():
     app.mongodb_client = MongoClient(config["ATLAS_URI"])
     app.database = app.mongodb_client[config["DB_NAME"]]
     print("Connected to the MongoDB database!")
 
 
-@app.on_event("shutdown")
 def shutdown_db_client():
     app.mongodb_client.close()
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup events
+    startup_b_client()
+    yield
+    # shutdown events
+    shutdown_db_client()
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(book_router, tags={"books"}, prefix="/book")
