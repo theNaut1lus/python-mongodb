9be49b6ab03c0c837b1d27e872a13add1f27c410
Sidney Aulakh <sidneyaulakh@gmail.com>
2024-11-11 22:27:56 +1100

    set lifespan using events

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from dotenv import dotenv_values
+from pymongo import MongoClient
+
+config = dotenv_values(".env")
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+def startup_b_client():
+    app.mongodb_client = MongoClient(config["ATLAS_URI"])
+    app.database = app.mongodb_client[config["DB_NAME"]]
+    print("Connected to the MongoDB database!")
+
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    app.mongodb_client.close()
